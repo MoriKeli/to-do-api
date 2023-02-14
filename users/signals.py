3da7d075dfab694c769af9cf6439fce6d229f7ce
brocode <kelimbth@gmail.com>
2023-02-14 21:55:34 +0300

    minor changes

--- users/signals.py
+++ users/signals.py
@@ -3,12 +3,6 @@ from django.dispatch import receiver
 from .models import Tasks
 import uuid
 
-
-@receiver(pre_save, sender=UserProfile)
-def generate_userProfile_ID(sender, instance, **kwargs):
-    if instance.id == "":
-        instance.id = str(uuid.uuid4()).replace('-', '')[:18]
-
 @receiver(pre_save, sender=Tasks)
 def generate_task_ID(sender, instance, **kwargs):
     if instance.id == "":
