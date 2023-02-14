9bc541a7cec43250d8b9df932c85319ee45c391b
brocode <kelimbth@gmail.com>
2023-02-14 21:38:18 +0300

    deleted default User model

--- users/models.py
+++ users/models.py
@@ -1,35 +1,5 @@
-from django.contrib.auth.models import User
 from django.db import models
-from PIL import Image
 
-class UserProfile(models.Model):
-    id = models.CharField(max_length=18, primary_key=True, editable=False, unique=True)
-    name = models.OneToOneField(User, on_delete=models.CASCADE, editable=False)
-    gender = models.CharField(max_length=7, blank=False)
-    phone_no = models.CharField(max_length=14, blank=False)
-    country = models.CharField(max_length=30, blank=False)
-    profile_pic = models.ImageField(upload_to='Users-Dps/', default='default.png')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ['name']
-        verbose_name_plural = 'Users Profiles'
-
-    def __str__(self):
-        return f'{self.name}'
-
-    def save(self, *args, **kwargs):
-        super(UserProfile, self).save(*args, **kwargs)
-
-        dp = Image.open(self.profile_pic.path)
-
-        if dp.height > 320 and dp.width > 320:
-            output_size = (320, 320)
-            dp.thumbnail(output_size)
-            dp.save(self.profile_pic.path)
-
-    
 class Tasks(models.Model):
     id = models.CharField(max_length=15, primary_key=True, unique=True, editable=False)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, editable=False)
--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,4 @@
 from django.db.models.signals import pre_save, post_save
-from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import UserProfile, Tasks
 import uuid
@@ -14,9 +13,3 @@ def generate_userProfile_ID(sender, instance, **kwargs):
 def generate_task_ID(sender, instance, **kwargs):
     if instance.id == "":
         instance.id = str(uuid.uuid4()).replace('-', '')[:15]
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        if instance.is_staff is False and instance.is_superuser is False:
-            UserProfile.objects.create(name=instance)
