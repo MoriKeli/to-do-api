d9f7197060d6e5e09f13abd88970ca76a112753b
brocode <kelimbth@gmail.com>
2023-02-14 20:59:44 +0300

    added a new URL path

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('create-new-task/', views.schedule_task_view, name='new_task'),
-    
+    path('task/<str:pk>/', views.edit_tasks_view, name='edit_task'),
 ]
