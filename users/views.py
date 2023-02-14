df9a92ecdd517a98d0a77dc47dfc2abbc7884a56
brocode <kelimbth@gmail.com>
2023-02-14 20:40:16 +0300

    added a view to schedule task

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .serializers import TasksSerializer
+from .models import Tasks
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def schedule_task_view(request):
+    if request.method == 'GET':
+        scheduled_task = get_object_or_404(Tasks)
+        serializer = TasksSerializer(scheduled_task)
+        
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = TasksSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
