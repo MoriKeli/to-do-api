4cdba856ac29cd818f7bbda059930f273fe67422
brocode <kelimbth@gmail.com>
2023-02-14 21:28:51 +0300

    registered tasks model in admin panel

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,8 @@ from .models import UserProfile, Tasks
 @admin.register(UserProfile)
 class UserProfileTable(admin.ModelAdmin):
     list_display = ['name', 'gender', 'country', 'phone_no']
-    readonly_fields = []
+    
+
+@admin.register(Tasks)
+class TasksTable(admin.ModelAdmin):
+    list_display = ['task', 'subtitle', 'created']
