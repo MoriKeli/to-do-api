3fe6b694fdf7005149940a117c678f836cdca15e
brocode <kelimbth@gmail.com>
2023-02-14 21:46:20 +0300

    removed table field

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Tasks(models.Model):
     id = models.CharField(max_length=15, primary_key=True, unique=True, editable=False)
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, editable=False)
     task = models.CharField(max_length=30, blank=False)
     subtitle = models.CharField(max_length=50, blank=True)
     created = models.DateTimeField(auto_now_add=True)
