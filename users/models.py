635b0685688346518f4771e5637c72dc31d2d39c
brocode <kelimbth@gmail.com>
2023-02-14 22:12:04 +0300

    changed table field

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Tasks(models.Model):
     id = models.CharField(max_length=15, primary_key=True, unique=True, editable=False)
-    task = models.CharField(max_length=30, blank=False)
+    task = models.CharField(max_length=70, blank=False)
     subtitle = models.CharField(max_length=50, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
