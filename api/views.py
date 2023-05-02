b0ef1efa1d7e9eb16230781e02bff1290b5b88a2
brocode <kelimbth@gmail.com>
2023-05-02 14:05:16 +0300

    created view to get and post tasks

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import TasksSerializer
+from .models import Tasks
 
-# Create your views here.
+class TasksView(APIView):
+    """
+        This view enables the user to view all scheduled task in the database and create a new task.
+    """
+    def get(self, request):
+        task = Tasks.objects.all()
+        serializer = TasksSerializer(task, many=True)
+
+        return Response(serializer.data)
+    
+
+    def post(self, request):
+        serializer = TasksSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
