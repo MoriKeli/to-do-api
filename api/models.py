c19ad8d2b66b448052af9b798fb0dd05e0adb605
brocode <kelimbth@gmail.com>
2023-05-02 19:11:03 +0300

    updated models

--- api/models.py
+++ api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Tasks(models.Model):
     id = models.CharField(max_length=10, primary_key=True, unique=True, editable=False)
     task = models.TextField(blank=False)    # scheduled task -> what the user is planning to do
+    description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
 
