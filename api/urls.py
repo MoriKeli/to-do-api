184c2e9b2d3535572be5c8dbffcccf90c161b952
brocode <kelimbth@gmail.com>
2023-05-02 14:24:11 +0300

    created view to update and delete tasks

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.TasksView.as_view(), name='all_tasks'),
+    path('update/<str:taskID>/', views.UpdateTasksView.as_view(), name='update_task'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -20,4 +20,35 @@ class TasksView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class UpdateTasksView(APIView):
+    """
+        This view enable a user to update and delete a task. The user will also view details of the selected task.
+    """
+    
+    def get_task(self, taskID):
+        """ This method gets a task object from the database using a primary key. """
+        try:
+            return Tasks.objects.get(id=taskID)
+        except Tasks.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, taskID):
+        serializer = TasksSerializer(self.get_task(taskID))
+        return Response(serializer.data)
+    
+    def put(self, request, taskID):
+        task = self.get_task(taskID)
+        serializer = TasksSerializer(task, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    
+    def delete(self, request, taskID):
+        task = self.get_task(taskID)
+        task.delete()
+        return Response(
+            {"message": "You deleted this task!"},
+            status=status.HTTP_204_NO_CONTENT
+        )
     
