ea9f3a6f59b58bac84cb15dbdb25fa9ecf6e4c29
brocode <kelimbth@gmail.com>
2023-05-02 13:39:37 +0300

    tasks db table

---
+++ api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Tasks(models.Model):
+    id = models.CharField(max_length=10, primary_key=True, unique=True, editable=False)
+    task = models.TextField(blank=False)    # scheduled task -> what the user is planning to do
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task
+
+    class Meta:
+        ordering = ['-created']
+        verbose_name_plural = 'Tasks'
